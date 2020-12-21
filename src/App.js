504563da681e5f9b00b27a7c10254378d5502ca4
martacamacho <martacamachovargas@gmail.com>
2020-12-21 22:09:42 +0100

    added as components: MovieList, MovieListHeading and Searchbox. started styling a bit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+require("dotenv").config();
 
-function App() {
+
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest = async () => {
+    const url = `http://www.omdbapi.com/?s=star wars&apikey=a55ab46b` /*process.env.API_KEY*/;
+    console.log(url, "url")
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  }
+
+  useEffect(() => {
+    getMovieRequest() ;
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid movie-app">
+    <div className="row d-flex align-items-center mt-4 mb-4">
+      <MovieListHeading heading="Movies" />
+      <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+    </div>
+      <div className="row">
+      <MovieList movies={movies}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SearchBox = (props) => {
+    return (
+        <div className="col col-sm-4">
+            <input className="form-control" value={props.value} 
+            onChange={(event) => props.setSearchValue(event.target.value)} 
+            placeholder="Search here your movie ...">
+            </input>
+        </div>
+    )
+}
+
+export default SearchBox
