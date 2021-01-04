9b338c0fb32357ac42c37a7f798b0e0ef30a275b
martacamacho <martacamachovargas@gmail.com>
2021-01-04 23:53:32 +0100

    fixing fetch bug on deploy

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   const [favourites, setFavourites] = useState ([]);
 
   const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a55ab46b`;
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=a55ab46b`;
     const response = await fetch(url);
     const responseJson = await response.json();
     if (responseJson.Search) {
